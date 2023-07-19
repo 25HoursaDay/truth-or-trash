879b9ca28ba3a7c6da0bcefea4c0b237ad2199fd
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-07-18 17:33:31 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
+var lower = 1;
+var upper = 5;
+var answer = Math.floor(Math.random() * (upper - lower)) + lower;
+console.log(document.getElementsByClassName("optionclass")[answer - 1].innerHTML);
 
